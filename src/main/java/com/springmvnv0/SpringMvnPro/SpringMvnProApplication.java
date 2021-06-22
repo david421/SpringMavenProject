5c22b11e90eadcafc4db4825a349f7168a685a7b
unknown <daiwei_421@hotmail.com>
2021-06-22 15:35:09 -0600

    first commit

---
+++ src/main/java/com/springmvnv0/SpringMvnPro/SpringMvnProApplication.java
@@ -0,0 +1,13 @@
+package com.springmvnv0.SpringMvnPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvnProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvnProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springmvnv0/SpringMvnPro/SpringMvnProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmvnv0.SpringMvnPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvnProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
