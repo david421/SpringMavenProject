5a71da9341b9dcf23fabb02249d00481253e554d
unknown <daiwei_421@hotmail.com>
2021-06-22 16:57:08 -0600

    add controller and properties file and prefix and suffix to properties file

--- src/main/java/com/springmvnv0/SpringMvnPro/controller/IndexController.java
+++ src/main/java/com/springmvnv0/SpringMvnPro/controller/IndexController.java
@@ -1,9 +1,9 @@
 package com.springmvnv0.SpringMvnPro.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 
 	@RequestMapping("/")
