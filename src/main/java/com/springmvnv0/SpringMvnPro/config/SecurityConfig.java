982d3983613a9d76527a92a56e767d5aaeadb3a3
unknown <daiwei_421@hotmail.com>
2021-08-21 14:56:08 -0600

    add original register page with controller

--- src/main/java/com/springmvnv0/SpringMvnPro/config/SecurityConfig.java
+++ src/main/java/com/springmvnv0/SpringMvnPro/config/SecurityConfig.java
@@ -3,11 +3,14 @@ package com.springmvnv0.SpringMvnPro.config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 //import org.springframework.security.core.userdetails.User;
 //import org.springframework.security.core.userdetails.User.UserBuilder;
 
@@ -62,6 +65,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.exceptionHandling().accessDeniedPage("/access-denied").and().csrf().disable();
 	}
 
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		
+		return new BCryptPasswordEncoder();
+	}
+	
 //	RequestMatcher csrfRequestMatcher = new RequestMatcher() {
 //        // Enabled CSFR protection on the following urls:
 //        //@formatter:off
